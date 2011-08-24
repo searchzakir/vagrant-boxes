8e6c07869cc36ddaafdfdfb2ae42fe0715716cfb
Lance Albertson <lance@osuosl.org>
2011-08-24 14:02:00 -0700

    Add vagrant boxes for more platforms

--- centos-puppet/init.sh
+++ centos-puppet/init.sh
@@ -2,8 +2,6 @@
 
 # module path
 cp /vagrant/puppet.conf /etc/puppet/puppet.conf
-# local binary files
-#cp -r /vagrant/packages/* /usr/portage/packages/
 # disable cfengine
 # NOTE: this will be taken care of by puppet eventually
 sed -i -e 's/.*cfexecd.*//g' /etc/crontab
---
+++ debian-puppet/init.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# module path
+cp /vagrant/puppet.conf /etc/puppet/puppet.conf
