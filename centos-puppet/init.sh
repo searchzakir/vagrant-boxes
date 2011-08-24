0d81b0d9483d88ebb49a2af85cff6b77464da088
Lance Albertson <lance@osuosl.org>
2011-08-24 16:50:42 -0700

    Add handy alias

--- centos-puppet/init.sh
+++ centos-puppet/init.sh
@@ -5,3 +5,4 @@ cp /vagrant/puppet.conf /etc/puppet/puppet.conf
 # disable cfengine
 # NOTE: this will be taken care of by puppet eventually
 sed -i -e 's/.*cfexecd.*//g' /etc/crontab
+echo 'alias p="puppet apply --modulepath=/vagrant/puppet/modules/core:/vagrant/puppet/modules/dist"' >> /root/.bashrc
--- debian-puppet/init.sh
+++ debian-puppet/init.sh
@@ -2,3 +2,4 @@
 
 # module path
 cp /vagrant/puppet.conf /etc/puppet/puppet.conf
+echo 'alias p="puppet apply --modulepath=/vagrant/puppet/modules/core:/vagrant/puppet/modules/dist"' >> /root/.bashrc
